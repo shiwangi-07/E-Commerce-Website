18bcb70f7457ca4c67939d18124e4b24d1e156b9
shiwangi-07 <shiwangi1204@gmail.com>
2024-05-15 15:43:42 +0530

    adding

--- frontend/src/components/HeaderComponent.js
+++ frontend/src/components/HeaderComponent.js
@@ -1,38 +1,84 @@
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
-
-const HeaderComponent = () => {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-};
-
-export default HeaderComponent;
-
+import {
+    Navbar,
+    Nav,
+    Container,
+    NavDropdown,
+    Badge,
+    Form,
+    DropdownButton,
+    Dropdown,
+    Button,
+    InputGroup,
+  } from "react-bootstrap";
+  
+  import { LinkContainer } from "react-router-bootstrap";
+  import { Link } from "react-router-dom";
+  
+  const HeaderComponent = () => {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">BEST ONLINE SHOP</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <InputGroup>
+                <DropdownButton id="dropdown-basic-button" title="All">
+                  <Dropdown.Item>Cars</Dropdown.Item>
+                  <Dropdown.Item>Electronics</Dropdown.Item>
+                  <Dropdown.Item>Books</Dropdown.Item>
+                </DropdownButton>
+                <Form.Control type="text" placeholder="Search in shop ..." />
+                <Button variant="warning">
+                  <i className="bi bi-search text-dark"></i>
+                </Button>
+              </InputGroup>
+            </Nav>
+            <Nav>
+              <LinkContainer to="/admin/orders">
+                <Nav.Link>
+                  Admin
+                  <span className="position-absolute top-1 start-10 translate-middle p-2 bg-danger border border-light rounded-circle"></span>
+                </Nav.Link>
+              </LinkContainer>
+  
+              <NavDropdown title="Shiwangi" id="collasible-nav-dropdown">
+                <NavDropdown.Item
+                  eventKey="/user/my-orders"
+                  as={Link}
+                  to="/user/my-orders"
+                >
+                  My orders
+                </NavDropdown.Item>
+                <NavDropdown.Item eventKey="/user" as={Link} to="/user">
+                  My profile
+                </NavDropdown.Item>
+                <NavDropdown.Item>Logout</NavDropdown.Item>
+              </NavDropdown>
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/register">
+                <Nav.Link>Register</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <Badge pill bg="danger">
+                    2
+                  </Badge>
+                  <i className="bi bi-cart-dash"></i>
+                  <span className="ms-1">CART</span>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  };
+  
+  export default HeaderComponent;
+  
+  
